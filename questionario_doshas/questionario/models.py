28f030957fd3084caf43c81f56d2a54e1bb1130a
lindahashimoto <hashimoto.hashimoto@yahoo.com>
2020-04-12 13:12:31 -0300

    My Django Girls app, first commit

---
+++ questionario_doshas/questionario/models.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+#O nome do app é questionario (nao é blog, como no exemplo do site)
+#depois eu preciso adaptar para o questionário de doshas
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
---
+++ questionario_doshas/questionario_doshas/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for questionario_doshas project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'questionario_doshas.settings')
+
+application = get_asgi_application()
